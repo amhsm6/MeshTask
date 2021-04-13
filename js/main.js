e08df68b47f5aec7a230f1eb7a173d40a1a6e44a
solmir36 <antonius.txt@gmail.com>
2021-04-13 22:22:55 +0300

    Commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 const video = document.getElementById('video');
 const canvas = document.getElementById('cnv');
 const showVideoBtn = document.getElementById('showVideo');
+const problem = document.getElementById('problem');
+const calc = document.getElementById('calc');
 
 const width = 560;
 const height = 400;
 
 showVideoBtn.addEventListener('click', async () => {
     try {
+        video.classList.remove('hidden');
         video.srcObject = await navigator.mediaDevices.getUserMedia({ video: { width, height }, audio: false });
         video.play();
     } catch (e) {
@@ -19,11 +22,16 @@ video.addEventListener('timeupdate', () => {
     ctx.drawImage(video, 0, 0, width, height);
 
     const im = cv.imread(canvas);
-    // TODO: detecting writing
+    // TODO: detecting input
     if (true) {
         video.pause();
         video.classList.add('hidden');
 
-
+        problem.value = '2x - 3 = 0';
     }
+});
+
+calc.addEventListener('click', () => {
+    const problem = problem.value;
+
 });
