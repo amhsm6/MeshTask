74bdc88791d3b87db7bdfe6bea0324bd8e9544d0
solmir36 <antonius.txt@gmail.com>
2021-04-13 22:44:27 +0300

    Commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
 const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
 
 gulp.task('scss', () => {
     return gulp.src('./scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS())
         .pipe(gulp.dest('./css'))
         .pipe(browserSync.stream());
 });
