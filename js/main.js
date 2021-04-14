56fcca8ec06b5695568c0216a1a61e6e9af818b7
Your Name <you@example.com>
2021-04-14 19:42:49 +0300

    Commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,50 @@
 onload = async () => {
+  await drawMainScreen(); 
+}
+
+
+function clearScreen() {
+  ROOT.innerHTML = '';
+}
+
+
+async function drawMainScreen() {
+  clearScreen();
+
+  ROOT.innerHTML = `
+<nav class="navbar is-transparent my-nav">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://uchebnik.mos.ru">
+      МЭШ
+    </a>
+  </div>
+
+  <div class="navbar-menu is-active">
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <a id="CAMERAS_BUTTON">Cameras</a>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+<div id="cameras_container" class="section container hidden"><div id="cameras" class="panel my-panel"></div></div>
+<canvas class="fullsize" id="cnv"></cnv>
+<video id="video" class="hidden"></video>
+  `;
+
   const video = document.getElementById('video');
   const canvas = document.getElementById('cnv');
+  const cameras = document.getElementById('cameras');
+  const camerasContainer = document.getElementById('cameras_container');
+  const camerasButton = document.getElementById('CAMERAS_BUTTON');
+  camerasButton.onclick = () => {
+    camerasContainer.classList.toggle('hidden');
+  };
+
+  canvas.width = canvas.clientWidth;
+  canvas.height = canvas.clientHeight;
 
   const ctx = canvas.getContext('2d');
 
@@ -8,9 +52,28 @@ onload = async () => {
   ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
   
   try {
-    const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
-    video.srcObject = stream;
-    video.play();
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    let idx = 1;
+    devices.forEach((device) => {
+      if (device.kind === 'videoinput') {
+        cameras.insertAdjacentHTML(
+          'beforeend', 
+          `
+            <a id=${ device.deviceId } class="panel-block">
+              Camera ${ idx }
+            </a>
+          `
+        );
+        idx++;
+        document.getElementById(device.deviceId).addEventListener('click', async () => {
+          camerasContainer.classList.toggle('hidden'); 
+          await processCamera(device.deviceId, ctx);
+        });
+      }
+    });
+
+
+    await processCamera(null, ctx);
   } catch (e) {
     console.error(e);
 
@@ -19,9 +82,20 @@ onload = async () => {
     document.body.appendChild(foo);
     foo.innerHTML = `${e.message}`;
   }
+}
 
+async function processCamera(deviceId, ctx) {
+  const stream = await navigator.mediaDevices.getUserMedia({ video: { deviceId }, audio: false });
+  video.srcObject = stream;
+  video.play();
+
+  const { width, height } = video.srcObject.getVideoTracks()[0].getSettings();
+  const ratio = width / height;
+  
   video.addEventListener('timeupdate', () => {
-    ctx.drawImage(video, 0, 0, video.width, video.height); 
+    ctx.drawImage(video, ctx.canvas.width / 2 - ratio * ctx.canvas.height / 2, 0, ratio * ctx.canvas.height, ctx.canvas.height);
+    const im = cv.imread(canvas);
+
   });
 }
 
