4b12564487270d0033b3e4a526665c80c614ed97
solmir36 <antonius.txt@gmail.com>
2021-04-15 09:11:17 +0300

    Commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,11 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
-const sass = require('gulp-sass');
-const cleanCSS = require('gulp-clean-css');
-
-gulp.task('scss', () => {
-    return gulp.src('./scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(cleanCSS())
-        .pipe(gulp.dest('./css'))
-        .pipe(browserSync.stream());
-});
 
 gulp.task('default', () => {
     browserSync.init({
-        server: "./"
+        server: './'
     });
 
-    gulp.watch('./scss/**/*.scss', gulp.series('scss'));
+    gulp.watch('./css/**/*.css').on('change', browserSync.reload);
     gulp.watch('./*.html').on('change', browserSync.reload);
 });
--- js/main.js
+++ js/main.js
@@ -22,8 +22,10 @@ async function drawMainScreen() {
   <div class="navbar-menu is-active">
     <div class="navbar-end">
       <div class="navbar-item">
+        <a id="about"><i class="big-font fa fa-question"></i></a>
+      </div>
+      <div class="navbar-item">
         <a id="CAMERAS_BUTTON"><i class="big-font fa fa-video-camera"></i></a>
-        </div>
       </div>
     </div>
   </div>
@@ -33,7 +35,8 @@ async function drawMainScreen() {
 <video id="video" class="hidden"></video>
   `;
 
-  const video = document.getElementById('video');
+  document.getElementById('about').addEventListener('click', () => showAbout())
+
   const canvas = document.getElementById('cnv');
   const cameras = document.getElementById('cameras');
   const camerasContainer = document.getElementById('cameras_container');
@@ -47,7 +50,7 @@ async function drawMainScreen() {
 
   const ctx = canvas.getContext('2d');
 
-  ctx.fillStyle = 'red';
+  ctx.fillStyle = 'white';
   ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
   
   try {
@@ -93,10 +96,12 @@ async function processCamera(deviceId, ctx) {
   
   const callback = () => {
     ctx.drawImage(video, ctx.canvas.width / 2 - ratio * ctx.canvas.height / 2, 0, ratio * ctx.canvas.height, ctx.canvas.height);
-    const im = cv.imread(ctx.canvas);
+
+    const rect = new cv.Rect(ctx.canvas.width / 2 - ratio * ctx.canvas.height / 2, 0, ratio * ctx.canvas.height, ctx.canvas.height);
+    const im = cv.imread(ctx.canvas).roi(rect);
 
     const mask = new cv.Mat();
-    const low = new cv.Mat(im.rows, im.cols, im.type(), [150, 50, 50, 0]);
+    const low = new cv.Mat(im.rows, im.cols, im.type(), [100, 100, 200, 0]);
     const high = new cv.Mat(im.rows, im.cols, im.type(), [255, 255, 255, 255]);
     cv.inRange(im, low, high, mask);
   
@@ -104,13 +109,14 @@ async function processCamera(deviceId, ctx) {
     const hierarchy = new cv.Mat();
     cv.findContours(mask, contours, hierarchy, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE);
 
-    if (!contours.size() || true) { 
-      // let max_area_idx = 0;
-      // for (let i = 0; i < contours.size(); i++) {
-      //   if (cv.contourArea(contours[i]) > cv.contourArea(max_area_idx)) {
-      //     max_area_idx = i;
-      //   }
-      // }
+    if (contours.size()) {
+      im.delete();
+      mask.delete();
+      low.delete();
+      high.delete();
+      hierarchy.delete();
+      contours.delete();
+
       video.pause();
       showResult();
     }
@@ -125,10 +131,63 @@ function showResult() {
   ROOT.innerHTML = `
 <nav class="navbar is-transparent my-nav">
   <div class="navbar-brand">
-    <a style="margin-left: 20px; margin-top: 10px;" class "navbar-item" id="back"><i class="fa fa-times big-font"></i></a> 
+    <a class="navbar-item" href="https://uchebnik.mos.ru">
+      МЭШ
+    </a>
+  </div>
+
+  <div class="navbar-menu is-active">
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <a class="navbar-item" id="back"><i class="fa fa-times big-font"></i></a>
+      </div>
+    </div>
   </div>
 </nav>
-<div class="container"
+<div class="container">
+  <div class="section" style="width: 60%; margin-left: 20%">
+    <div class="notification is-primary">
+      <h2 style="text-align: center">2x - 3 = 0</h2>
+    </div>
+  </div>
+  <div class="section">
+    <div class="notification is-primary">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti in natus nulla placeat. Ab aliquam aliquid assumenda at aut autem cum cupiditate, debitis eum ex fuga hic inventore iure iusto laboriosam libero maiores mollitia nam natus nesciunt nobis odio officia perspiciatis quam quasi quisquam quos rerum sint tempora temporibus veritatis voluptate voluptatem voluptates. Accusamus autem deserunt dolor eligendi excepturi fugiat ipsam, iure labore minima molestias nobis perspiciatis porro qui quisquam, ullam veritatis, voluptatum. Deleniti error iste minus officiis quo quod tempora tenetur unde vitae! Aliquam deleniti magnam nesciunt nisi praesentium quis repellendus sed similique sit voluptatibus. Eveniet id minus modi?</p>
+    </div>
+  </div>
 </div>
   `;
+
+  document.getElementById('back').addEventListener('click', () => drawMainScreen());
+}
+
+function showAbout() {
+  clearScreen();
+
+  ROOT.innerHTML = `
+<nav class="navbar is-transparent my-nav">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://uchebnik.mos.ru">
+      МЭШ
+    </a>
+  </div>
+
+  <div class="navbar-menu is-active">
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <a class="navbar-item" id="back"><i class="fa fa-times big-font"></i></a>
+      </div>
+    </div>
+  </div>
+</nav>
+<div class="container">
+  <div class="section">
+    <div class="notification is-primary">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti in natus nulla placeat. Ab aliquam aliquid assumenda at aut autem cum cupiditate, debitis eum ex fuga hic inventore iure iusto laboriosam libero maiores mollitia nam natus nesciunt nobis odio officia perspiciatis quam quasi quisquam quos rerum sint tempora temporibus veritatis voluptate voluptatem voluptates. Accusamus autem deserunt dolor eligendi excepturi fugiat ipsam, iure labore minima molestias nobis perspiciatis porro qui quisquam, ullam veritatis, voluptatum. Deleniti error iste minus officiis quo quod tempora tenetur unde vitae! Aliquam deleniti magnam nesciunt nisi praesentium quis repellendus sed similique sit voluptatibus. Eveniet id minus modi?</p>
+    </div>
+  </div>
+</div>
+  `;
+
+  document.getElementById('back').addEventListener('click', () => drawMainScreen());
 }
